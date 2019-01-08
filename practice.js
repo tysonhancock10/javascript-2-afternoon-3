60260a303e00a350c6ae17198f1ba0c271a8f22f
Tyson Hancock <tyson@Tysons-MacBook-Pro.local>
2019-01-08 15:21:18 -0700

    finished probles

--- practice.js
+++ practice.js
@@ -28,7 +28,7 @@
 */
 
 // Code Here 
-
+function first(arr,callback){callback(arr[0])}
 // Do not edit the code below.
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 
@@ -48,7 +48,9 @@ first(names, function(firstName){
 */
 
 //Code Here
-
+function last(arr, callback){
+  callback(arr[arr.length-1])
+}
 // Do not edit the code below.
 last(names, function(lastName){
   console.log('The last name in names is ' + lastName);
@@ -66,7 +68,9 @@ last(names, function(lastName){
 */
 
 //Code Here
-
+function multiply(num1, num2, callback){
+  callback(num1 * num2)
+}
 // Do not edit the code below.
 multiply(4, 3, function(answer){
   console.log('The answer is ' + answer); //should console.log 12
@@ -85,7 +89,15 @@ multiply(4, 3, function(answer){
 */
 
 //Code Here 
+function contains(arr, name, callback){
+  for(i=0; i<arr.length; i++){
+    if( name === arr[i]){
+      callback(true)
+    }
+    else {callback(false)}
 
+  }
+}
 // Do not edit the code below.
 contains(names, 'Colt', function(result){
   if(result === true){
@@ -106,7 +118,14 @@ contains(names, 'Colt', function(result){
 */
 
 //Code Here
-
+function uniq(arr, callback){
+  var newArray=[]
+  for(i=0; i<arr.length; i++){
+  if(newArray.includes(arr[i])=== false){
+    newArray.push(arr[i])
+  }
+callback(newArray)}
+}
 // Do not edit the code below.
 uniq(names, function(uniqArr){
   console.log('The new names array with all the duplicate items removed is ', uniqArr);
@@ -123,7 +142,12 @@ uniq(names, function(uniqArr){
 */
 
 //Code Here 
+function each(arr, cb){
+  for(i=0; i<arr.length; i++){
+    cb(arr[i], i)
+  }
 
+}
 // Do not edit the code below.
 each(names, function(item, indice){
   console.log('The item in the ' + indice + ' position is ' + item)
@@ -140,7 +164,13 @@ each(names, function(item, indice){
 */
 
 // Code here
-
+function getUserById(arr, ident, cb){
+  for(i=0; i<arr.length; i++){
+    if(arr[i].id === ident){
+      cb(arr[i])
+    }
+  }
+}
 // Do not edit the code below.
 var users = [
   {
